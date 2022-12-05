90c22a8e56070e5197aeebb473bc3f53e6abd609
Guahuan <zjukyj@163.com>
2022-12-05 16:38:01 +0800

    complete 3.2(no debug)

--- lab5/arch/riscv/kernel/buddy.c
+++ lab5/arch/riscv/kernel/buddy.c
@@ -1,16 +1,103 @@
 #include "buddy.h"
 
+#define IS_POWER_OF_2(N) ((N) > 0 && (((N) & ((N) - 1)) == 0))
+#define LEFT_LEAF(INDEX) ((INDEX) * 2 + 1)
+#define RIGHT_LEAF(INDEX) ((INDEX) * 2 + 2)
+#define PARENT(INDEX) (((INDEX) - 1) / 2)
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
+
+#define BUDDY_SIZE 32
+buddy mybuddy;
+extern unsigned long long text_start;
+
+// the root's index is 0
 void init_buddy_system(void)
 {
+    unsigned node_size;
+
+    mybuddy.size = BUDDY_SIZE;
+    node_size = BUDDY_SIZE * 2;
+
+    for (int i = 0; i < 2 * BUDDY_SIZE - 1; i++) {
+        if (IS_POWER_OF_2(i + 1))
+            node_size /= 2;
+        mybuddy.bitmap[i] = node_size;
+    }
     return;
 }
 
 void *alloc_pages(int npages)
 {
-    return;
+    unsigned index = 0;
+    unsigned node_size;
+    unsigned offset = 0;
+
+    // if uninited
+    if (mybuddy.size == 0)
+        return -1;
+
+    // fix npages
+    if (npages <= 0)
+        npages = 1;
+    else if (!IS_POWER_OF_2(npages)) {
+        unsigned ret = 1;
+        while (ret < npages)
+            ret = ret << 1;
+        npages = ret;
+    }
+
+    // unenough place
+    if (mybuddy.bitmap[index] < npages)
+        return -1;
+
+    // find the node index
+    for(node_size = mybuddy.size; node_size != npages; node_size /= 2 ) {
+        if (mybuddy.bitmap[LEFT_LEAF(index)] >= npages)
+            index = LEFT_LEAF(index);
+        else
+            index = RIGHT_LEAF(index);
+    }
+
+    // get offset index of pages
+    mybuddy.bitmap[index] = 0;
+    offset = (index + 1) * node_size - mybuddy.size;
+
+    // trace back
+    while (index) {
+        index = (int)PARENT(index);
+        mybuddy.bitmap[index] = MAX(mybuddy.bitmap[LEFT_LEAF(index)], mybuddy.bitmap[RIGHT_LEAF(index)]);
+    }
+    return (unsigned long long)((unsigned long long)&text_start + 0x1000 * offset);
 }
 
 void free_pages(void* addr)
 {
-    return;
+    unsigned node_size, index = 0;
+    unsigned left_bitmap, right_bitmap;
+
+    unsigned offset = ((unsigned long long)addr - (unsigned long long)&text_start) / (unsigned long long)0x1000;
+
+    node_size = 1;
+    index = offset + mybuddy.size - 1;
+
+    for(; mybuddy.bitmap[index]; index = PARENT(index)) {
+        node_size *= 2;
+        if (index == 0)
+            return;
+    }
+
+    mybuddy.bitmap[index] = node_size;
+
+    while (index) {
+        index = PARENT(index);
+        node_size *= 2;
+
+        left_bitmap = mybuddy.bitmap[LEFT_LEAF(index)];
+        right_bitmap = mybuddy.bitmap[RIGHT_LEAF(index)];
+
+        if (left_bitmap + right_bitmap == node_size)
+            mybuddy.bitmap[index] = node_size;
+        else
+            mybuddy.bitmap[index] = MAX(left_bitmap, right_bitmap);
+    }
 }
--- lab5/arch/riscv/kernel/vm.c
+++ lab5/arch/riscv/kernel/vm.c
@@ -8,12 +8,106 @@ extern unsigned long long text_start;
 extern unsigned long long rodata_start;
 extern unsigned long long data_start;
 extern unsigned long long _end;
+extern buddy mybuddy;
 
 #define ValidBit(i) ((i)&1)
 #define PPN(i) (((i) >> 10) & 0xfffffffffff)
 #define offset (0xffffffe000000000 - 0x80000000)
 
-void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz,
-                    int perm) {}
+void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz, int perm)
+{
+    /* your code */
+    for (uint64_t va_i = va, pa_i = pa; va_i < va + sz; va_i += (uint64_t)0x1000, pa_i += (uint64_t)0x1000) {
+        /* 寻找页表地址 */
+        // 获得三级vpn
+        uint64_t vpn2 = (uint64_t)((va_i >> 30) & (0x1ff));
+        uint64_t vpn1 = (uint64_t)((va_i >> 21) & (0x1ff));
+        uint64_t vpn0 = (uint64_t)((va_i >> 12) & (0x1ff));
+        // 三级页表基地址
+        uint64_t *pgtbl2 = pgtbl, *pgtbl1, *pgtbl0;
+        // 存放pte地址
+        uint64_t *pte;
 
-void paging_init() {}
+        /* 处理第二级页表 */
+        // 获得pte
+        pte = &pgtbl2[vpn2];
+        // 如果 V bit 合法
+        if (*pte & 0x1) {
+            uint64_t ppn = (uint64_t)((*pte >> 10) & 0x7ffffff);
+            pgtbl1 = (uint64_t*)(ppn << 12);
+        }
+        // 如果 V bit 不合法，则说明此页表不存在，需要从物理内存里分配空间
+        else {
+            // 生成新物理地址
+            uint64_t new_page_address = (uint64_t)alloc_pages(1);
+            // 更新当前pte内容
+            // 保留原来pte的前10位，reserved
+            uint64_t reserved = (uint64_t)(*pte & 0xffc0000000000000);
+            // 使用新物理地址右移12位生成ppn，然后再左移十位放到有效位置
+            uint64_t ppn = (uint64_t)((uint64_t)(new_page_address >> 12) << 10);
+            // 生成最后十位，除最后1位（V，有效位）为1外，其余为0
+            uint64_t protection = (uint64_t)0x1;
+            // 更新pte
+            *pte = reserved | ppn | protection;
+            // 更新页表基地址
+            pgtbl1 = (uint64_t*)new_page_address;
+        }
+
+        /* 处理第一级页表 */
+        // 获得pte
+        pte = &pgtbl1[vpn1];
+        // 如果 V bit 合法
+        if (*pte & 0x1) {
+            uint64_t ppn = (uint64_t)((*pte >> 10) & 0x7ffffff);
+            pgtbl0 = (uint64_t*)(ppn << 12);
+        }
+        // 如果 V bit 不合法，则说明此页表不存在，需要从物理内存里分配空间
+        else {
+            // 生成新物理地址
+            uint64_t new_page_address = (uint64_t)alloc_pages(1);
+            // 更新当前pte内容
+            // 保留原来pte的前10位，reserved
+            uint64_t reserved = (uint64_t)(*pte & 0xffc0000000000000);
+            // 使用新物理地址右移12位生成ppn，然后再左移十位放到有效位置
+            uint64_t ppn = (uint64_t)((uint64_t)(new_page_address >> 12) << 10);
+            // 生成最后十位，除最后1位（V，有效位）为1外，其余为0
+            uint64_t protection = (uint64_t)0x1;
+            // 更新pte
+            *pte = reserved | ppn | protection;
+            // 更新页表基地址
+            pgtbl0 = (uint64_t*)new_page_address;
+        }
+
+        /* 处理第零级页表 */
+        pte = &pgtbl0[vpn0];
+        // 保留原来pte的前10位，reserved
+        uint64_t reserved = (uint64_t)((*pte) & 0xffc0000000000000);
+        // 从pa_i获得ppn，并放到有效位置
+        uint64_t ppn = (uint64_t)((uint64_t)((pa_i >> 12)) << 10);
+        // 生成最后十位，最后1位为1，其余位为perm
+        uint64_t protection = (uint64_t)(perm << 1) | (uint64_t)0x1;
+        // 更新pte
+        *pte = reserved | ppn | protection;
+    }
+}
+
+void paging_init()
+{
+    init_buddy_system();
+    uint64_t kernel_pages = ((uint64_t)&_end - (uint64_t)&text_start) / (uint64_t)0x1000;
+    alloc_pages(kernel_pages);
+
+    /* your code */
+    uint64_t *pgtbl = &_end;
+
+    //text section
+    create_mapping(pgtbl, (uint64_t)&text_start + offset, (uint64_t)&text_start, (uint64_t)(0x2000), 1 | 4);
+    //rodata section
+    create_mapping(pgtbl, (uint64_t)&rodata_start + offset, (uint64_t)&rodata_start, (uint64_t)(0x1000), 1);
+    //other(except text & rodata)
+    create_mapping(pgtbl, (uint64_t)&data_start + offset, (uint64_t)&data_start, (uint64_t)(0x1000000-0x1000-0x2000), 1 | 2);
+    //内核起始地址等值映射
+    create_mapping(pgtbl, (uint64_t)(0x80000000), (uint64_t)0x80000000, (uint64_t)(0x1000), 1 | 2 | 4);
+    //UART
+    create_mapping(pgtbl, (uint64_t)(0x10000000), (uint64_t)(0x10000000), (uint64_t)(1), 1 | 2 | 4);
+}
